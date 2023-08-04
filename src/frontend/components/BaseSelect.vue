1297ed5f27b457489c7ef26c5b6233bf7e895d7e
Phat Nguyen <phatnguyen@gmx.de>
2023-08-04 08:49:59 +0200

    feat: cp

--- src/frontend/components/BaseSelect.vue
+++ src/frontend/components/BaseSelect.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { onClickOutside } from '@vueuse/core'
-
 defineProps<{
   placeholder: string
   options: {
@@ -17,19 +15,18 @@ function handleItemClick(value: string) {
   isExpanded.value = false
 }
 
-const button = ref<HTMLButtonElement>()
-onClickOutside(button, () => isExpanded.value = false)
+const isMouseOverOpts = ref(false)
 </script>
 
 <template>
   <div class="relative">
     <button
-      ref="button"
       type="button"
       role="combobox"
       :aria-expanded="isExpanded"
       class="h-10 w-full flex items-center justify-between border border-input rounded-md bg-transparent px-3 py-2 text-sm ring-offset-background disabled:cursor-not-allowed placeholder:text-muted-foreground disabled:opacity-50 focus:outline-none"
       @click="isExpanded = !isExpanded"
+      @blur="isExpanded = false"
     >
       <span style="pointer-events: none">
         <slot name="label">
@@ -39,20 +36,29 @@ onClickOutside(button, () => isExpanded.value = false)
       <span class="i-carbon-caret-down" />
     </button>
 
+    <!-- <select v-model="modelValue" class="absolute h-1px w-1px overflow-hidden border-none p-0 -m-1px" aria-hidden="true" tabindex="-1" style="clip: rect(0px, 0px, 0px, 0px);">
+      <option value="" />
+      <option v-for="{ label, value } in options" :key="value" :value="value">
+        {{ label }}
+      </option>
+    </select> -->
+
     <Transition
-      enter-active-class="transition-all origin-top-right"
-      leave-active-class="transition-all origin-top-right"
-      enter-from-class="opacity-100 scale-100"
-      leave-to-class="opacity-0 scale-90"
+      enter-active-class="transition-opacity"
+      leave-active-class="transition-opacity"
+      enter-from-class="opacity-0"
+      leave-to-class="opacity-0"
     >
       <ul
         v-if="isExpanded"
-        class="absolute right-0 top-full mt-1 w-full border border-input rounded-md bg-background p-1 text-sm ring-offset-background disabled:cursor-not-allowed divide-y-1 divide-border disabled:opacity-50"
+        class="absolute right-0 top-full z-2 mt-1 w-full border border-input rounded-md bg-background p-1 text-sm ring-offset-background disabled:cursor-not-allowed disabled:opacity-50"
+        @mouseover="isMouseOverOpts = true"
+        @mouseleave="isMouseOverOpts = false"
       >
         <li
           v-for="{ label, value } in options" :key="label"
           class="flex items-center gap-x-2 rounded-md px-3 py-2 hover:(bg-secondary text-secondary-foreground)"
-          :class="[modelValue === value ? 'bg-secondary text-secondary-foreground' : 'bg-background']"
+          :class="[!isMouseOverOpts && modelValue === value ? 'bg-secondary text-secondary-foreground' : 'bg-background']"
           role="button"
           @click="handleItemClick(value)"
         >
