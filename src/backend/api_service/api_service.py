ec3af4af8ce4e8f415961387af6f68ba34f7de62
ChrisItisdud <chris.reichel@gmx.net>
2023-08-20 20:49:59 +0200

    minor changes and moved files

--- src/backend/api_service/api_service.py
+++ src/backend/api_service/api_service.py
@@ -17,20 +17,44 @@ TODO: Decide how to behave if SONG_NOT_RECOGNISED happens!
 # constant for the API key. TODO: allow user to provide API key.
 API_KEY = "b'kDPSCEb4"
 
+
 class SongOptionResult(Enum):
     """
     SongOptionResult contains information about the state of the API service.
     """
-    SONG_EXTENDED = 0 # analysed song has results matching with the previous song, so the previous song is extended. Happens if a song was split in the middle.
-    SONG_FINISHED = 1 # analysed song has no results matching with the previous song, so the previous song can be retrieved using get_last_song().
+
+    SONG_EXTENDED = 0  # analysed song has results matching with the previous song, so the previous song is extended. Happens if a song was split in the middle.
+    SONG_FINISHED = 1  # analysed song has no results matching with the previous song, so the previous song can be retrieved using get_last_song().
     # commented out for now because AcoustID doesn't support this
     # FINGERPRINT_MISMATCH = 2 # fingerprints from the start and end of the analysed song have no matching results. Happens if two songs are still together after splitting.
-    SONG_NOT_RECOGNISED = 3 # analysed song has no results from the API. Happens when the song isn't yet in the AcoustID database.
+    SONG_NOT_RECOGNISED = 3  # analysed song has no results from the API. Happens when the song isn't yet in the AcoustID database.
+
 
 lastSongData = np.array([[], []])
-lastSongMetadataOptions = [{"id": "", "title": "not-set", "album": "not-set", "artist": "not-set", "album-artist": "not-set", "year": "not-set", "number": 0}]
+lastSongMetadataOptions = [
+    {
+        "id": "",
+        "title": "not-set",
+        "album": "not-set",
+        "artist": "not-set",
+        "album-artist": "not-set",
+        "year": "not-set",
+        "number": 0,
+    }
+]
 currentSongData = np.array([[], []])
-currentSongMetadataOptions = [{"id": "", "title": "not-set", "album": "not-set", "artist": "not-set", "album-artist": "not-set", "year": "not-set", "number": 0}]
+currentSongMetadataOptions = [
+    {
+        "id": "",
+        "title": "not-set",
+        "album": "not-set",
+        "artist": "not-set",
+        "album-artist": "not-set",
+        "year": "not-set",
+        "number": 0,
+    }
+]
+
 
 def get_last_song():
     """
@@ -39,6 +63,7 @@ def get_last_song():
     :returns: a tuple that contains the song's audio data and the song's metadata options.
     """
     return lastSongData, lastSongMetadataOptions
+
 
 def get_song_options(songData, samplerate):
     """
@@ -50,11 +75,13 @@ def get_song_options(songData, samplerate):
     global currentSongData
     duration, fingerprint = create_fingerprint(songData, samplerate)
     metadata = get_api_song_data(fingerprint, duration)
-    if(len(metadata) == 0):
+    if len(metadata) == 0:
         store_finished_song()
         return SongOptionResult.SONG_NOT_RECOGNISED
 
-    matchMetadataOptions = get_overlapping_metadata_values(currentSongMetadataOptions, metadata)
+    matchMetadataOptions = get_overlapping_metadata_values(
+        currentSongMetadataOptions, metadata
+    )
     if len(matchMetadataOptions) == 0:
         store_finished_song(songData=songData, metadataOptions=metadata)
         return SongOptionResult.SONG_FINISHED
@@ -62,6 +89,7 @@ def get_song_options(songData, samplerate):
         currentSongMetadataOptions = matchMetadataOptions
         currentSongData = np.concatenate((currentSongData, songData), axis=1)
         return SongOptionResult.SONG_EXTENDED
+
 
 def get_overlapping_metadata_values(metadata1, metadata2):
     """
@@ -72,7 +100,7 @@ def get_overlapping_metadata_values(metadata1, metadata2):
     """
     if len(metadata1) == 0:
         return metadata2
-    elif len(metadata2)  ==  0:
+    elif len(metadata2) == 0:
         return metadata1
     else:
         overlappingMetadata = {}
@@ -81,6 +109,7 @@ def get_overlapping_metadata_values(metadata1, metadata2):
             if metadata["id"] in idsEnd:
                 overlappingMetadata.add(metadata)
         return overlappingMetadata
+
 
 def create_fingerprint(songData, samplerate):
     """
@@ -92,11 +121,12 @@ def create_fingerprint(songData, samplerate):
     """
     filename = "TEMP_FILE_FOR_FINGERPRINTING"
 
-    saveNumPyAsAudioFile(songData, filename, os.path.abspath('') + '\\', rate=samplerate)
-    filenameWithPath = os.path.abspath(filename + '.mp3')
+    saveNumPyAsAudioFile(songData, os.path.abspath(filename), "", rate=samplerate)
+    filenameWithPath = os.path.abspath(filename + ".mp3")
     fingerprintDuration, fingerprint = acoustid.fingerprint_file(filenameWithPath)
     os.remove(filenameWithPath)
     return (fingerprintDuration, fingerprint)
+
 
 def get_api_song_data(fingerprint, fingerprintLength):
     """
@@ -107,14 +137,18 @@ def get_api_song_data(fingerprint, fingerprintLength):
     """
     try:
         result = []
-        for score, recording_id, title, artist in acoustid.parse_lookup_result(acoustid.lookup(API_KEY, fingerprint, fingerprintLength)):
-            result.append({"score": score, "id": recording_id, "title": title, "artist": artist})
+        for score, recording_id, title, artist in acoustid.parse_lookup_result(
+            acoustid.lookup(API_KEY, fingerprint, fingerprintLength)
+        ):
+            result.append(
+                {"score": score, "id": recording_id, "title": title, "artist": artist}
+            )
         return result
     except acoustid.WebServiceError:
         return []
-    return []
 
-def store_finished_song(songData = np.array([[],[]]), metadataOptions = ()):
+
+def store_finished_song(songData=np.array([[], []]), metadataOptions=()):
     """
     Store the current (finished) song in lastSongData/lastSongMetadataOptions to be retrieved using get_last_song(), store the provided song data as the current song data or reset it.
     :param songData: The new currently read song's data. Resets to empty array if not specified.
