2b3b1d45ad23fa9f6f2596892a62cf71e93533ca
Erhard, Josua <josua.erhard@student.fhws.de>
2023-08-27 13:05:39 +0200

    adjust parameters for segmentation and refactor main method

--- src/modules/segmentation.py
+++ src/modules/segmentation.py
@@ -7,15 +7,32 @@ import numpy as np
 from scipy import signal
 import librosa
 from librosa import feature
-import matplotlib.pyplot as plt
 
 from audio_stream_io import readAudiofileToStream, saveNumPyAsAudioFile, overlappingStream
 
 
+# TODO: Implement debugging mode (plots, prints) ?
+
+
 class Feature(Enum):
+    """
+    This enum represent the different features we can extract and use for splitting.
+    """
     CHROMA = 1
     SPECTRAL = 2
     MFCC = 3
+
+
+# TODO: (Optional) Rework this to Preset and set more values than down-sampling
+class Downsampling(Enum):
+    """
+    This enum represent the Downsampling for the SSM.
+    """
+    STRICT = 4
+    NORMAL = 8
+    LENIANT = 16
+    EXTRA_LENIANT = 32
+    # CUSTOM = 0 this should be specified by the user
 
 
 def create_checkerboard_kernel(n: int):
@@ -57,7 +74,7 @@ def create_gaussian_checkerboard_kernel(n: int, var=1.0, normalize=True):
     return kernel
 
 
-def smooth_downsample_feature_sequence(x, sr, filt_len, down_sampling, w_type='boxcar'):
+def smooth_downsample_feature_sequence(x, sr, filt_len, down_sampling):
     """
         Smooths and down-samples a given feature-sequence and its samplerate.
 
@@ -65,12 +82,11 @@ def smooth_downsample_feature_sequence(x, sr, filt_len, down_sampling, w_type='b
         :param sr: the samplerate of the feature sequence
         :param filt_len: length of the smoothing filter
         :param down_sampling: down-sampling rate
-        :param w_type: ???
 
         :returns: the smoothed and down-sampled feature sequence, the down-sampled samplerate
     """
 
-    filt_kernel = np.expand_dims(signal.get_window(w_type, filt_len), axis=0)
+    filt_kernel = np.expand_dims(signal.get_window('boxcar', filt_len), axis=0)
     x_smooth = signal.convolve(x, filt_kernel, mode='same') / filt_len
     x_smooth = x_smooth[:, ::down_sampling]
     sr_feature = sr / down_sampling
@@ -101,13 +117,11 @@ def median_downsample_feature_sequence(x, sr, filt_len, down_sampling):
     return x_smooth, sr_feature
 
 
-def normalize_feature_sequence(x, threshold=0.0001, v=None):
+def normalize_feature_sequence(x):
     """
         Normalized a given feature sequence.
 
         :param x: the feature sequence to normalize
-        :param threshold: ???
-        :param v: ???
 
         :returns: the normalized feature sequence
     """
@@ -115,12 +129,10 @@ def normalize_feature_sequence(x, threshold=0.0001, v=None):
     k, n = x.shape
     x_norm = np.zeros((k, n))
 
-    if v is None:
-        v = np.ones(k, dtype=np.float64) / np.sqrt(k)
-
+    v = np.ones(k, dtype=np.float64) / np.sqrt(k)
     for i in range(n):
         s = np.sqrt(np.sum(x[:, i] ** 2))
-        if s > threshold:
+        if s > 0.001:
             x_norm[:, i] = x[:, i] / s
         else:
             x_norm[:, i] = v
@@ -128,7 +140,7 @@ def normalize_feature_sequence(x, threshold=0.0001, v=None):
     return x_norm
 
 
-def compute_self_similarity(feature, sr, filter_len=41, down_sampling=32, norm_threshold=0.001):
+def compute_self_similarity(feature, sr, filter_len=41, down_sampling=8):
     """
         Computes the self similarity matrix for a given feature sequence.
         Stacks the feature sequence with delay, smooths and down-samples it and finally normalizes the sequence before
@@ -138,7 +150,6 @@ def compute_self_similarity(feature, sr, filter_len=41, down_sampling=32, norm_t
         :param sr: the sample-rate
         :param filter_len: length for the filter kernel, needs to be odd ()
         :param down_sampling: down-sampling rate for feature sequence (default: 32)
-        :param norm_threshold: threshold for normalization (default: 0.001)
 
         :returns: the self similarity matrix, the resulting sample-rate
     """
@@ -148,19 +159,20 @@ def compute_self_similarity(feature, sr, filter_len=41, down_sampling=32, norm_t
     # feature smoothing
     chroma, down_sampled_sr = smooth_downsample_feature_sequence(chroma, sr, filt_len=filter_len, down_sampling=down_sampling)
     # normalization
-    chroma = normalize_feature_sequence(chroma, threshold=norm_threshold)
+    chroma = normalize_feature_sequence(chroma)
 
     # compute self similarity matrix
     ssm = np.dot(np.transpose(chroma), chroma)
 
-    plt.imshow(ssm, cmap='magma')
-    plt.colorbar()
-    plt.show()
+    # Debug Plotting
+    # plt.imshow(ssm, cmap='magma')
+    # plt.colorbar()
+    # plt.show()
 
     return ssm, down_sampled_sr
 
 
-def compute_novelty_ssm(ssm, kernel=None, n=10, var=0.5, exclude=False):
+def compute_novelty_ssm(ssm, kernel=None, n=8, var=0.5, exclude=False):
     """
         Computes the novelty function for the given self similarity matrix.
 
@@ -180,12 +192,11 @@ def compute_novelty_ssm(ssm, kernel=None, n=10, var=0.5, exclude=False):
     N = ssm.shape[0]
     M = 2 * n + 1
     nov = np.zeros(N)
-    s_padded = np.pad(ssm, n, mode='symmetric')
+    s_padded = np.pad(ssm, n, mode='reflect')
 
     for i in range(N):
         nov[i] = np.sum(s_padded[i:i + M, i:i + M] * kernel)
 
-    # TODO: Try without normalization
     # Normalize to [0.0 - 1.0]
     nov = (nov - np.min(nov)) / (np.max(nov) - np.min(nov))
 
@@ -198,6 +209,7 @@ def compute_novelty_ssm(ssm, kernel=None, n=10, var=0.5, exclude=False):
     return nov
 
 
+# TODO: Needs more research (maybe adaptive thresholding)
 def select_peaks(novelty, peak_threshold=0.5, down_sampling=32, offset=0):
     """
         Selects the peak of the given function based on the given threshold.
@@ -209,7 +221,6 @@ def select_peaks(novelty, peak_threshold=0.5, down_sampling=32, offset=0):
         :returns: all indexes where the function peaks
     """
 
-    # TODO: Needs more research (maybe adaptive thresholding)
     # Find peaks
     # peaks = librosa.util.peak_pick(x=novelty, pre_max=3, post_max=3, pre_avg=3, post_avg=5, delta=0.5, wait=10)
     peaks, _ = signal.find_peaks(novelty, prominence=peak_threshold)
@@ -230,19 +241,28 @@ def select_peaks(novelty, peak_threshold=0.5, down_sampling=32, offset=0):
     # peaks = np.insert(peaks, 0, 0)
     # peaks = np.append(peaks, len(novelty))
 
-
-    plt.plot(novelty)
-    for x in peaks:
-        plt.vlines(x, ymin=0, ymax=0.01, colors='red', label=f'Peak: {x}')
-    plt.show()
+    # Debug plotting
+    # plt.plot(novelty)
+    # for x in peaks:
+    #     plt.vlines(x, ymin=0, ymax=0.01, colors='red', label=f'Peak: {x}')
+    # plt.show()
 
     peaks *= down_sampling
-    # peaks[-1] -= 1
     peaks += int(offset)
     return peaks
 
 
 def extract_chroma(x, sr, hop_length, fft_window=2048):
+    """
+    Extracts the chroma feature vector from the given sequence.
+
+    :param x: The sequence to work on.
+    :param sr: The sample-rate of the sequence
+    :param hop_length: The hop-length of the sequence.
+    :param fft_window: The Window size for the fast-fourier-transformation (default: 2048)
+
+    :return: The chroma feature vector.
+    """
     # convert to mono
     x_mono = librosa.to_mono(x)
     # extract chroma feature
@@ -250,12 +270,23 @@ def extract_chroma(x, sr, hop_length, fft_window=2048):
 
 
 def extract_spectro(x, sr, hop_length, fft_window=2048):
+    """
+    Extracts the mel-spectrogram feature vector from the given sequence.
+
+    :param x: The sequence to work on.
+    :param sr: The sample-rate of the sequence
+    :param hop_length: The hop-length of the sequence.
+    :param fft_window: The Window size for the fast-fourier-transformation (default: 2048)
+
+    :return: The mel-spectrogram feature vector.
+    """
     # convert to mono
     x_mono = librosa.to_mono(x)
     # extract mel-spectrogram
     return librosa.feature.melspectrogram(y=x_mono, sr=sr, hop_length=hop_length, center=False, n_fft=fft_window)
 
 
+# TODO: Reevaluate this; didn't seem to work well
 def extract_mfcc(x, sr):
     # convert to mono
     x_mono = librosa.to_mono(x)
@@ -263,7 +294,7 @@ def extract_mfcc(x, sr):
     return librosa.feature.mfcc(y=x_mono, sr=sr)
 
 
-def segment_block(block, sr, hop_length, feature: Feature, filter_len=41, down_sampling=32, threshold=0.5, offset=0):
+def segment_block(block, sr, hop_length, feature: Feature, filter_len=41, down_sampling=8, threshold=0.7, offset=0):
     """
         Segments a data array into segments, where each segment represents a different part in the audio.
 
@@ -274,7 +305,7 @@ def segment_block(block, sr, hop_length, feature: Feature, filter_len=41, down_s
         :returns: a list of indexes, where transitions should be
     """
 
-    # TODO: Test with more features (currently mostly chroma)
+    # Spectral worked best so far
     if feature == Feature.CHROMA:
         feature_seq = extract_chroma(block, sr, hop_length, fft_window=2048)
     elif feature == Feature.SPECTRAL:
@@ -284,39 +315,35 @@ def segment_block(block, sr, hop_length, feature: Feature, filter_len=41, down_s
     else:
         raise TypeError("Illegal Feature Value.")
 
-    # TODO: Test more parameters
-    ssm, _ = compute_self_similarity(feature_seq, sr, filter_len=filter_len, down_sampling=down_sampling, norm_threshold=0.001)
-    nov = compute_novelty_ssm(ssm, n=8, exclude=True)
+    ssm, _ = compute_self_similarity(feature_seq, sr, filter_len=filter_len, down_sampling=down_sampling)
+    nov = compute_novelty_ssm(ssm, n=8, exclude=False)
     return select_peaks(nov, peak_threshold=threshold, down_sampling=down_sampling, offset=offset)
 
 
-def filter_peaks(peaks, n=2):
+def filter_peaks(peaks, n=3):
+    """
+    Filters a given vector to values that appear at least n times.
+
+    :param peaks: The given vector
+    :param n: The minimum number of times a value has to appear
+
+    :return: The filtered vector
+    """
     unique, counts = np.unique(peaks, return_counts=True)
     return np.sort([k for k, v in dict(zip(unique, counts)).items() if v >= n])
 
 
-# TODO: Move this elsewhere
-if __name__ == '__main__':
-    ####################################################################################################################
-    # TODO: Change this
-    for f in os.listdir('../../test_output/'):
-        os.remove(os.path.join('../../test_output/', f))
+def segment_file(path: path, down_sampling=Downsampling.NORMAL):
+    """
+    Segments a given file into a generator.
 
-    file_parent_path = path.abspath('../../../../Music/Stuff/')
+    :param path: THe Path to the File
+    :param down_sampling: The down-sampling rate for the SSM see: :class:`Downsampling`
 
-    album = "full_album.mp3"  # 1h 6m 30s    13 songs
-    hiphop = "hiphop.mp3"  # 15m 30s      4 songs
-    edm = "modern_edm.mp3"  # 20m 40s      5 songs
-    pop = "old_pop.mp3"  # 19m 45s      5 songs
-    rock = "rock.mp3"  # 15m 40s      4 songs
-    crackle = "with_crackle.mp3"  # 12m          3 songs
-    # This contains short snippets of songs, most of the transitions are interludes -> very noisy / difficult
-    yorushika = "Yorushika_Album_Trailer.mp3"  # 10m          15 songs ?
-
-    current_track = path.join(file_parent_path, crackle)
-    ####################################################################################################################
-    block_len = int(4096)
-    stream, sr, hop_length = readAudiofileToStream(current_track, block_len=block_len)
+    :return: A generator that iterates over the found segments, the start time and duration for the original file
+    """
+    block_len = 4096
+    stream, sr, hop_length = readAudiofileToStream(path, block_len=block_len)
 
     transitions = np.zeros(1)
     last_frame_in_audiofile = 0
@@ -326,21 +353,48 @@ if __name__ == '__main__':
             continue
 
         offset = idx * block_len * 0.25
-        transitions = np.append(transitions, segment_block(block, sr, hop_length, Feature.SPECTRAL, down_sampling=16, threshold=0.7, offset=offset))
+        transitions = np.append(transitions,
+                                segment_block(block, sr, hop_length, Feature.SPECTRAL,
+                                              down_sampling=down_sampling.value, threshold=0.7, offset=offset))
+
         last_frame_in_audiofile = librosa.core.samples_to_frames(block.shape[1], hop_length=hop_length) + offset
 
+    # Filter peaks and insert boundaries
     transitions = filter_peaks(transitions, n=3)
     transitions = np.insert(transitions, 0, 0)
     transitions = np.append(transitions, last_frame_in_audiofile - 1)
 
-    for (index, (start, end)) in enumerate(pairwise(transitions)):
+    for start, end in pairwise(transitions):
         start_time = librosa.core.frames_to_time(start, sr=sr, hop_length=hop_length, n_fft=2048)
         duration = librosa.core.frames_to_time(end - start, sr=sr, hop_length=hop_length, n_fft=2048)
 
-        segment, sr = librosa.load(current_track, mono=False, sr=sr, offset=start_time, duration=duration)
+        yield librosa.load(current_track, mono=False, sr=sr, offset=start_time, duration=duration), start_time, duration
 
+
+# TODO: Move this elsewhere
+if __name__ == '__main__':
+    # TODO: This whole segment is only for testing purposes and should be refactored
+    ####################################################################################################################
+    for f in os.listdir('../../test_output/'):
+        os.remove(os.path.join('../../test_output/', f))
+
+    file_parent_path = path.abspath('../../../../Music/Stuff/')
+
+    album = "full_album.mp3"      # 1h 6m 30s    13 songs
+    hiphop = "hiphop.mp3"         # 15m 30s      4 songs
+    edm = "modern_edm.mp3"        # 20m 40s      5 songs
+    pop = "old_pop.mp3"           # 19m 45s      5 songs
+    rock = "rock.mp3"             # 15m 40s      4 songs
+    crackle = "with_crackle.mp3"  # 12m          3 songs
+
+    current_track = path.join(file_parent_path, crackle)
+    ####################################################################################################################
+
+    for (segment, sr), start, duration in segment_file(current_track, Downsampling.NORMAL):
+        start_m, start_s = divmod(int(start), 60)
+        m, s = divmod(int(duration), 60)
+        song_name = f'Song_{start_m:02d}m{start_s:02d}s_{m:02d}m{s:02d}s'
         # probably not how this would work
-        song_name = f'Song_{index}'
         # tags = identify(segment)
         # song_name = tags.name
         # tag_audio_file(tags)
@@ -348,6 +402,5 @@ if __name__ == '__main__':
         output_path = path.abspath('../../test_output/')
         saveNumPyAsAudioFile(segment, song_name, output_path, sr)
 
-        m, s = divmod(int(duration), 60)
         print(f'Successfully written {song_name} to {output_path} with a ' +
               f'duration of {m:02d}m {s:02d}s.')
