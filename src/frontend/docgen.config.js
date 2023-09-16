80b0027cb4c5e15544114a520989ff9959498553
phatnguyen <phatnguyen@gmx.de>
2023-09-16 15:04:01 +0200

    docs: fix

--- src/frontend/docgen.config.js
+++ src/frontend/docgen.config.js
@@ -7,10 +7,12 @@ module.exports = defineConfig({
   outDir: 'docs/frontend/components',
   templates: {
     props(props) {
-      let md = `| Name      | Description | Type | Default |\n | ----------- | ----------- |----------- |----------- |
-      `
+      let md = '| Name | Description | Type | Default |\n | ----------- | ----------- |----------- |----------- |\n'
+
       props.forEach(({ name, description, required, type, defaultValue, tags }) => {
-        let typeCol = type.elements?.length ? type.elements.map(e => e.name).join(', ') : type.name
+        let typeCol = type.elements?.length
+          ? type.elements.map(e => e.name.replaceAll('\"', '\'')).join(', ')
+          : type.name
         if (!required)
           typeCol += ' (optional)'
 
@@ -18,10 +20,6 @@ module.exports = defineConfig({
       })
 
       return md
-    },
-    defaultExample(doc) {
-      console.log(doc)
-      return doc
     },
   },
 })
