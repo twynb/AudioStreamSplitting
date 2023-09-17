8af3af975c7594222fb150e6071e2797fc7dc080
4lex0017 <alex.kern@live.de>
2023-09-17 11:39:11 +0200

    changed from subprocess.call to os.system

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,4 +1,3 @@
-import subprocess
 import platform
 import pathlib
 import os
@@ -6,17 +5,17 @@ import os
 file = pathlib.Path(__file__)
 
 if platform.system() == "Windows":
-    subprocess.call(
+    os.system(
         f"sphinx-apidoc -d 2 -f -e -l -P -o .\\docs\\backend .\\src\\backend\\ /*/tests/*"
     )
     make = file.parent.joinpath("backend", "make.bat").resolve().as_posix()
 else:
-    subprocess.call(
-        f"sphinx-apidoc -d 2 -f -e -l -P -o docs/backend src/backend/ /*/tests/*"
+    os.system(
+        f"sphinx-apidoc -d 2 -f -e -l -P -o ./docs/backend/ ./src/backend/ **/tests/*"
     )
     make = "make"
 
 os.remove(file.parent.joinpath("backend", "modules.rst").resolve().as_posix())
-subprocess.call(f"{make} clean")
-subprocess.call(f"{make} html")
-subprocess.call(f"{make} markdown")
+os.system(f"{make} clean")
+os.system(f"{make} html")
+os.system(f"{make} markdown")
