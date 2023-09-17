484f047cafd547a85d3d2307f9a83de4c41ae53d
phat.nguyen <89086602+chubetho@users.noreply.github.com>
2023-09-17 15:16:29 +0200

    Record (#71)
    * feat: record
    
    * fix: res status
    
    * docs: fix
    
    * docs: fix
    
    * refactor: record
    
    * i18n fix
    
    * UUID type fix
    
    ---------
    
    Co-authored-by: Christina / Itisdud / twyn!b <92167981+ChrisItisdud@users.noreply.github.com>
    Co-authored-by: ChrisItisdud <chris.reichel@gmx.net>

--- src/backend/api/project.py
+++ src/backend/api/project.py
@@ -1,9 +1,12 @@
 import datetime
 import os
+import subprocess
 import uuid
 
 from flask import Blueprint, jsonify, make_response, request
+from utils.logger import log_error
 from utils.path import audios_dir, mkdir
+from werkzeug.utils import secure_filename
 
 project_bp = Blueprint("project", __name__)
 
@@ -14,7 +17,7 @@ def create():
     description = request.form.get("description")
     files = request.files.getlist("file")
     project_id = uuid.uuid4()
-    project_path = f"{audios_dir}/{project_id}"
+    project_path = os.path.join(audios_dir, str(project_id))
     mkdir(project_path)
 
     project = {
@@ -27,11 +30,35 @@ def create():
     }
 
     for file in files:
-        file_name = file.filename
-        name = file_name.split(".")[0]
-        file_type = file_name.split(".")[-1]
-        file_path = f"{project_path}/{file_name}"
+        file_name = secure_filename(file.filename)
+        name, file_extension = os.path.splitext(file_name)
+        file_type = file_extension.strip(".")
+        file_path = os.path.join(project_path, file_name)
         file.save(file_path)
+
+        if file_extension.lower() == ".webm":
+            output_path = os.path.join(project_path, f"{name}.wav")
+            try:
+                subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-i",
+                        file_path,
+                        "-acodec",
+                        "pcm_s16le",
+                        "-ar",
+                        "44100",
+                        output_path,
+                    ],
+                    check=True,
+                )
+            except subprocess.CalledProcessError as e:
+                log_error(e)
+            else:
+                os.remove(file_path)
+                file_name = f"{name}.wav"
+                file_path = output_path
+                file_type = "wav"
 
         project["files"].append(
             {
@@ -43,6 +70,21 @@ def create():
         )
 
     return jsonify(project)
+
+
+@project_bp.route("/check-ffmpeg")
+def check_ffmpeg():
+    try:
+        subprocess.run(
+            ["ffmpeg", "-version"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+        return "ffmpeg is installed", 200
+    except subprocess.CalledProcessError as e:
+        log_error(e)
+        return "ffmpeg is not installed", 404
 
 
 @project_bp.route("/clear", methods=["GET"])
--- src/frontend/components/dialogs/BaseModal.vue
+++ src/frontend/components/dialogs/BaseModal.vue
@@ -4,6 +4,7 @@ import { VueFinalModal } from 'vue-final-modal'
 defineProps<{
   title?: string
   contentClass?: string
+  preventClose?: boolean
 }>()
 
 const { driver } = useDriver()
@@ -14,7 +15,8 @@ const { driver } = useDriver()
     overlay-class="-z-1 bg-#00000080 inset-0 pointer-events-none absolute"
     class="fixed inset-0 flex-center"
     :content-class="`rounded-sm p-6 bg-primary-foreground focus:outline-none border border-border ${contentClass}`"
-    :esc-to-close="driver.isActive() ? false : true"
+    :esc-to-close="driver.isActive() ? false : !preventClose"
+    :click-to-close="!preventClose"
   >
     <slot name="header">
       <h2 class="text-2xl">
