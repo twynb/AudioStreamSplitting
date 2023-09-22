5cb720f4bbef669345ab802199f78627e8de7501
ChrisItisdud <chris.reichel@gmx.net>
2023-09-22 22:41:36 +0200

    make backend docs work with vitepress

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,6 +1,19 @@
-import platform
-import pathlib
 import os
+import pathlib
+import platform
+import re
+
+
+def replace_dot_with_underscore(matchobj: re.Match):
+    result = (
+        "("
+        + matchobj.group(1).replace(".", "_")
+        + ".md"
+        + (matchobj.group(2) if matchobj.group(2) is not None else "")
+        + ")"
+    )
+    return result
+
 
 file = pathlib.Path(__file__)
 
@@ -20,3 +33,29 @@ os.remove(file.parent.joinpath("backend", "modules.rst").resolve().as_posix())
 os.system(f"{make} clean")
 os.system(f"{make} html")
 os.system(f"{make} markdown")
+
+print(
+    "Finished generating documentation, renaming and editing files so they work with vitepress..."
+)
+
+folder = file.parent.joinpath("backend", "_build", "markdown")
+files = [f for f in os.listdir(folder) if f.endswith(".md")]
+
+for file_name in files:
+    file_path = os.path.join(str(folder), file_name)
+    file_content = ""
+    with open(file_path, mode="r", encoding="utf8") as file:
+        file_content = file.read()
+    os.remove(file_path)
+
+    result_file_name = file_name.replace(".", "_").replace("_md", ".md")
+    result_file_path = os.path.join(str(folder), result_file_name)
+
+    content_pattern = re.compile(r"\(([A-Za-z\._]+)\.md(#[A-Za-z\.\-_]+)?\)")
+    result_content = content_pattern.sub(replace_dot_with_underscore, file_content)
+
+    with open(result_file_path, mode="w", encoding="utf8") as result_file:
+        result_file.seek(0)
+        result_file.write(result_content)
+
+print("Finished renaming files!")
