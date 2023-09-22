3a87c8564cebfc34bdb0e8b72fe88e9936606df5
ChrisItisdud <chris.reichel@gmx.net>
2023-09-22 23:17:49 +0200

    add regex comments

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -48,9 +48,17 @@ for file_name in files:
         file_content = file.read()
     os.remove(file_path)
 
+    # no need for a regex here, we can just replace all "." and then fix the file ending
     result_file_name = file_name.replace(".", "_").replace("_md", ".md")
     result_file_path = os.path.join(str(folder), result_file_name)
 
+    """
+    match all module links. links in markdown files are formatted as
+    [Display Text](module.submodule.file_name.md#section.to.access) - This will match the
+    section in normal brackets, and the sub() call will replace all ``.`` in the file
+    names with ``_``. The section after the # is not affected.
+    The output for the above example would be ``(module_submodule_file_name.md#section.to.access)``.
+    """
     content_pattern = re.compile(r"\(([A-Za-z\._]+)\.md(#[A-Za-z\.\-_]+)?\)")
     result_content = content_pattern.sub(replace_dot_with_underscore, file_content)
 
