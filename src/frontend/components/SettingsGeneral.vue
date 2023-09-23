575345acd3f9b161c184473d76ac75ffdf43693b
phatnguyen <phatnguyen@gmx.de>
2023-09-23 23:38:14 +0200

    chore: remove viewtransition

--- src/frontend/components/SettingsGeneral.vue
+++ src/frontend/components/SettingsGeneral.vue
@@ -6,14 +6,7 @@ const { t } = useI18n()
 const { currentLocale } = useLocale()
 const localOpts = Object.entries(LangMap).map(([key, value]) => ({ label: value, value: key }))
 
-const { isDark, toggle } = useDarkToggle()
-
-function handleToggleDark() {
-  if (!document.startViewTransition)
-    toggle()
-
-  document.startViewTransition(() => toggle())
-}
+const { isDark } = useDarkToggle()
 </script>
 
 <template>
@@ -48,48 +41,8 @@ function handleToggleDark() {
           </p>
         </div>
 
-        <BaseSwitch v-model="isDark" :on-switch="handleToggleDark" />
+        <BaseSwitch v-model="isDark" />
       </div>
     </div>
   </div>
 </template>
-
-<style>
-::view-transition-group(root) {
-  animation-duration: 1s;
-}
-::view-transition-new(root),
-::view-transition-old(root) {
-  mix-blend-mode: normal;
-}
-
-::view-transition-new(root) {
-  animation-name: reveal-light;
-}
-
-::view-transition-old(root),
-.dark::view-transition-old(root) {
-  animation: none;
-}
-.dark::view-transition-new(root) {
-  animation-name: reveal-dark;
-}
-
-@keyframes reveal-dark {
-  from {
-    clip-path: polygon(-30% 0, -30% 0, -15% 100%, -10% 115%);
-  }
-  to {
-    clip-path: polygon(-30% 0, 130% 0, 115% 100%, -10% 115%);
-  }
-}
-
-@keyframes reveal-light {
-  from {
-    clip-path: polygon(130% 0, 130% 0, 115% 100%, 110% 115%);
-  }
-  to {
-    clip-path: polygon(130% 0, -30% 0, -15% 100%, 110% 115%);
-  }
-}
-</style>
